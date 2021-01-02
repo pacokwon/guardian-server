72fe0808a83525857936f0c9c128e09024944b0f
pacokwon <haechank@gmail.com>
2021-01-02 17:00:51 +0900

    wip: express dependency + skeleton controller and route codes

---
+++ src/routes/comment.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import * as cc from '../controllers/comment.controller';
+
+const router = express.Router();
+
+router.put('/:id', cc.modifyComment);
+router.delete('/:id', cc.removeComment);
+
+export default router;
