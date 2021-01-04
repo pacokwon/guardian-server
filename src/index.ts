a2ff6e3c5fb0e3ed9aaee3c22ae965a4b6e9a64e
pacokwon <haechank@gmail.com>
2021-01-04 18:15:45 +0900

    bugfix: create connection before app listen

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import { createConnection } from 'typeorm';
 import app from './app';
 
 const PORT = app.get('port');
-app.listen(PORT, () => {
-    console.log(`HTTP server listening at port ${PORT}`)
-})
+
+createConnection()
+    .then(_ => {
+        app.listen(PORT, () => {
+            console.log(`HTTP server listening at port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Server terminated with error:', err);
+    });
