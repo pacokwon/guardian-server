f40be3ade45f45af5fc3485eeed2ddc87acd7142
pacokwon <haechank@gmail.com>
2021-01-04 11:38:19 +0900

    refactor: apply path aliases to existing codebase where appropriate

--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as cc from '../controllers/comment.controller';
+import * as cc from '@controllers/comment.controller';
 
 const router = express.Router();
 
