77af6330836233d73fb7e56819b6edfff4181a68
pacokwon <haechank@gmail.com>
2021-01-05 19:14:17 +0900

    add: tsoa interface for user table

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,19 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * @tsoaModel
+ */
+export interface IUser {
+    /**
+     * @maxLength 30
+     */
+    nickname: string;
+    /**
+     * @isInt
+     */
+    deleted: number;
+}
+
 @Entity({ name: 'User' })
 export class User {
     @PrimaryGeneratedColumn()
