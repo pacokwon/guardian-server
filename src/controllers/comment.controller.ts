873be70b87aeca4c0fe105ea6c8842789616dc0d
pacokwon <haechank@gmail.com>
2021-01-06 16:23:11 +0900

    update: remove unimplemented controllers and rename user controller to pascal case

--- src/controllers/comment.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-const modifyComment = (req: Request, res: Response) => {};
-const removeComment = (req: Request, res: Response) => {};
-
-export { modifyComment, removeComment };
