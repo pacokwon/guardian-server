0dba585dd9cdfe13ee2a7d90485b3853626c6fb7
pacokwon <haechank@gmail.com>
2021-01-06 15:41:21 +0900

    update: test code according to modified endpoint specification

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,93 +1,76 @@
 import { createConnection, getConnection } from 'typeorm';
 import request from 'supertest';
-import app from '../app';
+import app from '@/app';
 
 describe('/api/user endpoint test', () => {
     beforeAll(async () => {
-        await createConnection();
+        const connection = await createConnection();
+        await connection.synchronize(true);
     });
 
     afterAll(async () => {
-        const connection = getConnection();
-        const queryRunner = connection.createQueryRunner();
-
-        await queryRunner.dropTable('comments');
-        await queryRunner.dropTable('pets');
-        await queryRunner.dropTable('users');
-        await connection.close();
+        await getConnection().close();
     });
 
     it('should retrieve an empty list of users', async () => {
         const response = await request(app).get('/api/user');
         expect(response.status).toBe(200);
-        expect(response.body.users).toStrictEqual([]);
+        expect(response.body).toStrictEqual([]);
     });
 
     it('should successfully create two new users "foo" and "bar"', async () => {
-        const response1 = await request(app)
+        const createFooResponse = await request(app)
             .post('/api/user')
-            .send({ username: 'foo' });
-        expect(response1.status).toBe(200);
-        expect(response1.body.success).toBe(true);
+            .send({ nickname: 'foo' });
+        expect(createFooResponse.status).toBe(200);
 
-        const response2 = await request(app)
+        const createBarResponse = await request(app)
             .post('/api/user')
-            .send({ username: 'bar' });
-        expect(response2.status).toBe(200);
-        expect(response2.body.success).toBe(true);
+            .send({ nickname: 'bar' });
+        expect(createBarResponse.status).toBe(200);
     });
 
     it('should retrieve a list with two users "foo" and "bar"', async () => {
         const response = await request(app).get('/api/user');
         const { status } = response;
-        const { users } = response.body;
+        const users = response.body;
 
         expect(status).toBe(200);
         expect(users).toHaveLength(2);
-        expect(users).toContainEqual({ username: 'foo', deleted: 0 });
-        expect(users).toContainEqual({ username: 'bar', deleted: 0 });
+
+        expect(users).toContainEqual({ id: 1, nickname: 'foo', deleted: 0 });
+        expect(users).toContainEqual({ id: 2, nickname: 'bar', deleted: 0 });
     });
 
     it('should retrieve the desired user', async () => {
-        const response = await request(app).get('/api/user/foo');
-        const { user } = response.body;
+        const response = await request(app).get('/api/user/1');
 
         expect(response.status).toBe(200);
-        expect(user.username).toBe('foo');
-    });
-
-    it('should fail to modify a username to a one that already exists', async () => {
-        const response = await request(app)
-            .put('/api/user/foo')
-            .send({ username: 'bar' });
-
-        expect(response.status).toBe(400);
-        expect(response.body.success).toBe(false);
+        expect(response.body.user.nickname).toBe('foo');
     });
 
     it('should successfully modify foo\'s nickname to "baz"', async () => {
-        const response1 = await request(app)
-            .put('/api/user/foo')
-            .send({ username: 'baz' });
+        const modifyFooResponse = await request(app)
+            .put('/api/user/1')
+            .send({ nickname: 'baz' });
 
-        expect(response1.status).toBe(200);
-        expect(response1.body.success).toBe(true);
+        expect(modifyFooResponse.status).toBe(200);
+        expect(modifyFooResponse.body.success).toBe(true);
 
-        const response2 = await request(app).get('/api/user/baz');
-        const { user } = response2.body;
+        const getBazResponse = await request(app).get('/api/user/1');
 
-        expect(response2.status).toBe(200);
-        expect(user.username).toBe('baz');
+        expect(getBazResponse.status).toBe(200);
+        expect(getBazResponse.body.user.nickname).toBe('baz');
     });
 
     it('should successfully delete an existing user "bar"', async () => {
-        const response = await request(app).delete('/api/user/bar');
+        const response = await request(app).delete('/api/user/1');
         expect(response.status).toBe(200);
         expect(response.body.success).toBe(true);
     });
 
     it('should not retreive information of a deleted user "bar"', async () => {
-        const response = await request(app).get('/api/user/bar');
+        const response = await request(app).get('/api/user/1');
         expect(response.status).toBe(400);
         expect(response.body.user).toBeNull();
     });
