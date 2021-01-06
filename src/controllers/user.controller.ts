e5f0340b03093c90aa9c366e604b2941b4a34c73
pacokwon <haechank@gmail.com>
2021-01-06 15:20:25 +0900

    bugfix: use correct type for request body

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,14 @@ import { IUser } from '@/entities/User';
 import * as UserService from '@/services/UserService';
 
 @Route('user')
+interface UserCreationRequestBody {
+    nickname: string;
+}
+
+interface UserModificationRequestBody {
+    nickname: string;
+}
+
 export class UserController extends Controller {
     @Get()
     async getAllUsers(): Promise<IUser[]> {
@@ -23,16 +31,20 @@ export class UserController extends Controller {
     }
 
     @Post()
-    async createUser(@Body() nickname: string): Promise<void> {
+    async createUser(
+        @Body() requestBody: UserCreationRequestBody
+    ): Promise<void> {
+        const { nickname } = requestBody;
         await UserService.createUser(nickname);
         this.setStatus(200);
     }
 
     @Put('{id}')
     async modifyUser(
-        @Body() nickname: string,
+        @Body() requestBody: UserModificationRequestBody,
         @Path() id: number
     ): Promise<{ success: boolean }> {
+        const { nickname } = requestBody;
         // as of now, the only modifiable data in a user is the nickname
         const success = await UserService.modifyNickname(id, nickname);
 
