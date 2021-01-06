85de89725f280b3e6be5d77b5649e3e5719ae91b
pacokwon <haechank@gmail.com>
2021-01-06 10:41:58 +0900

    update: controller and service methods according to new user schema

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,65 +1,48 @@
 import { getRepository } from 'typeorm';
-import User from '@/entities/User';
+import { User } from '@/entities/User';
 
 const getAll = async (): Promise<User[]> => {
     return await getRepository(User).find({ where: { deleted: 0 } });
 };
 
-const getSingleUser = async (username: string): Promise<User | null> => {
+const getSingleUser = async (id: number): Promise<User | null> => {
     const user = await getRepository(User).findOne({
-        where: {
-            username,
-            deleted: 0
-        }
+        where: { id, deleted: 0 }
     });
 
     if (!user) return null;
     return user;
 };
 
-const exists = async (username: string): Promise<boolean> => {
-    const count = await getRepository(User).count({ username });
-    return count > 0;
-};
-
-const createUser = async (username: string): Promise<void> => {
+const createUser = async (nickname: string): Promise<void> => {
     const repository = getRepository(User);
-    const user = repository.create({ username });
+    const user = repository.create({ nickname });
     await repository.save(user);
 };
 
-const modifyUsername = async (
-    oldUsername: string,
-    newUsername: string
+const modifyNickname = async (
+    id: number,
+    newNickname: string
 ): Promise<boolean> => {
-    const newUsernameExists = await exists(newUsername);
-    if (newUsernameExists) return false;
-
-    const user = await getSingleUser(oldUsername);
+    const user = await getSingleUser(id);
 
     if (!user) return false;
 
-    user.username = newUsername;
+    user.nickname = newNickname;
     await getRepository(User).save(user);
     return true;
 };
 
-const removeUser = async (username: string): Promise<boolean> => {
-    const user = await getSingleUser(username);
+const removeUser = async (id: number): Promise<boolean> => {
+    const user = await getSingleUser(id);
 
     // cannot remove a user that does not exist
     if (!user || user.deleted === 1) return false;
 
     user.deleted = 1;
     await getRepository(User).save(user);
+
     return true;
 };
 
-export {
-    getAll,
-    getSingleUser,
-    exists,
-    createUser,
-    modifyUsername,
-    removeUser
-};
+export { getAll, getSingleUser, createUser, modifyNickname, removeUser };
