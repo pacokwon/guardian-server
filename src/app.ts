f44e8602bec19e43b7d358eb1ce0b3ac7b8b33d9
pacokwon <haechank@gmail.com>
2021-01-06 16:17:47 +0900

    feat: serve swagger docs from /docs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 import { RegisterRoutes } from './routes';
 
 const app = express();
@@ -7,7 +8,11 @@ const app = express();
 app.use(morgan('combined'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 RegisterRoutes(app);
+app.use('/docs', swaggerUi.serve, async (_: Request, res: Response) => {
+    res.send(swaggerUi.generateHTML(await import('../docs/swagger.json')));
+});
 
 app.set('port', process.env.PORT || 3000);
 
