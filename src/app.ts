4a0cfb0427304e2c30a8f4d2e176ae29ea46c886
pacokwon <haechank@gmail.com>
2021-01-12 15:53:54 +0900

    add: middleware for error handling

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import { RegisterRoutes } from './routes';
+import { errorHandler } from '@/middleware/errorHandler';
 
 const app = express();
 
@@ -11,9 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 RegisterRoutes(app);
+
 app.use('/docs', swaggerUi.serve, async (_: Request, res: Response) => {
     res.send(swaggerUi.generateHTML(await import('../docs/swagger.json')));
 });
+app.use(errorHandler);
 
 app.set('port', process.env.PORT || 3000);
 
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,16 @@
+import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
+import { ApiError } from '@/common/error';
+
+// reference: handling errors in express
+// https://expressjs.com/en/guide/error-handling.html
+export const errorHandler: ErrorRequestHandler = (
+    error: ApiError,
+    _request: Request,
+    res: Response,
+    _next: NextFunction
+) => {
+    res.json({
+        status: error.status,
+        message: error.message
+    });
+};
