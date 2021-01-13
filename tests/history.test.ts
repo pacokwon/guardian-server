669f74634f046cb2f899e3965d029df2c2fcf6b9
pacokwon <haechank@gmail.com>
2021-01-13 10:39:21 +0900

    add: tests for two history related endpoints

--- tests/history.test.ts
+++ tests/history.test.ts
@@ -14,13 +14,12 @@ describe('/api/pets/:id/users + /api/users/:id/pets endpoint test', () => {
         await pool.query(`ALTER TABLE User AUTO_INCREMENT=1`);
         await pool.query(`ALTER TABLE Pet AUTO_INCREMENT=1`);
 
-        // create 3 users
+        // create 4 users
         await pool.query(
-            `INSERT INTO User (nickname) VALUES ('bob'), ('joe'), ('max')`
+            `INSERT INTO User (nickname) VALUES ('bob'), ('joe'), ('max'), ('jay')`
         );
 
         // create 5 pets
-        // species nickname imageUrl
         await pool.query(`
             INSERT INTO Pet (species, nickname, imageUrl) VALUES
             ('cat', 'foo', 'https://placekitten.com/300/300'),
@@ -37,24 +36,92 @@ describe('/api/pets/:id/users + /api/users/:id/pets endpoint test', () => {
 
     it('should successfully register bob, joe, max as guardians for baz, ham, egg respectively', async () => {
         const registerBobToBazResponse = await request(app)
-            .post('/api/pets/3')
+            .post('/api/pets/3/users')
             .send({
-                id: 1
+                userID: 1
             });
         expect(registerBobToBazResponse.status).toBe(201);
 
         const registerJoeToHamResponse = await request(app)
-            .post('/api/pets/4')
+            .post('/api/pets/4/users')
             .send({
-                id: 2
+                userID: 2
             });
         expect(registerJoeToHamResponse.status).toBe(201);
 
         const registerMaxToEggResponse = await request(app)
-            .post('/api/pets/5')
+            .post('/api/pets/5/users')
             .send({
-                id: 3
+                userID: 3
             });
         expect(registerMaxToEggResponse.status).toBe(201);
+    });
+
+    it('should not register user for currently registered pet', async () => {
+        const registerJoeToBazResponse = await request(app)
+            .post('/api/pets/3/users')
+            .send({
+                userID: 2
+            });
+        expect(registerJoeToBazResponse.status).toBe(400);
+    });
+
+    it('should not proceed registration with unknown user or pet', async () => {
+        const registerUnknownUserResponse = await request(app)
+            .post('/api/pets/1/users')
+            .send({
+                userID: 10
+            });
+        expect(registerUnknownUserResponse.status).toBe(404);
+
+        const registerUnknownPetResponse = await request(app)
+            .post('/api/pets/10/users')
+            .send({
+                userID: 1
+            });
+        expect(registerUnknownPetResponse.status).toBe(404);
+    });
+
+    it('should not proceed registration with unknown user or pet', async () => {
+        const registerUnknownUserResponse = await request(app)
+            .post('/api/pets/1/users')
+            .send({
+                userID: 10
+            });
+        expect(registerUnknownUserResponse.status).toBe(404);
+
+        const registerUnknownPetResponse = await request(app)
+            .post('/api/pets/10/users')
+            .send({
+                userID: 1
+            });
+        expect(registerUnknownPetResponse.status).toBe(404);
+    });
+
+    it('should successfully unregister an existing registration', async () => {
+        const unregisterBobResponse = await request(app).delete(
+            '/api/pets/3/users/1'
+        );
+
+        expect(unregisterBobResponse.status).toBe(200);
+    });
+
+    it('should not proceed unregistration for invalid registration', async () => {
+        const unregisterBobResponse = await request(app).delete(
+            '/api/pets/3/users/1'
+        );
+        expect(unregisterBobResponse.status).toBe(404);
+    });
+
+    it('should not proceed unregistration for unknown pet or user', async () => {
+        const unregisterBobResponse = await request(app).delete(
+            '/api/pets/5/users/1'
+        );
+        expect(unregisterBobResponse.status).toBe(404);
+
+        const unregisterJayResponse = await request(app).delete(
+            '/api/pets/4/users/4'
+        );
+        expect(unregisterJayResponse.status).toBe(404);
     });
 });
