a115fadec4dc6f3a8622c9b8849fc273b35d8858
pacokwon <haechank@gmail.com>
2021-01-15 17:11:08 +0900

    add: better error handling for tsoa validation errors

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,16 +1,28 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
+import { ValidateError } from 'tsoa';
 import { ApiError } from '@/common/error';
 
 // reference: handling errors in express
 // https://expressjs.com/en/guide/error-handling.html
 export const errorHandler: ErrorRequestHandler = (
-    error: ApiError,
-    _req: Request,
+    error: unknown,
+    req: Request,
     res: Response,
-    _next: NextFunction
+    next: NextFunction
 ) => {
-    const status = error.status || 500;
-    const message = error.message || 'Internal Server Error';
+    if (error instanceof ValidateError) {
+        console.warn(`Caught Validation Error for ${req.path}:`, error.fields);
+        return res.status(400).json({
+            message: 'Validation Failed',
+            details: error?.fields
+        });
+    }
 
-    res.status(status).json({ message });
+    if (error instanceof ApiError) {
+        const status = error?.status || 500;
+        const message = error?.message || 'Internal Server Error';
+        return res.status(status).json({ message });
+    }
+
+    next();
 };
