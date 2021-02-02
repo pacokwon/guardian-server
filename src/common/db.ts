12ed91e8b773b3bb6df7e3eac758cfb90f853c48
pacokwon <haechank@gmail.com>
2021-02-02 16:24:52 +0900

    add: specify db host in environment variable

--- src/common/db.ts
+++ src/common/db.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { createPool, Pool } from 'mysql2/promise';
 
 const pool = createPool({
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.NODE_ENV === 'test' ? 'GuardianTest' : 'Guardian'
