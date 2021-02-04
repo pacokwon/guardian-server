faeedbbfb1c68d919e9386e8c392e31f547a0cd0
pacokwon <haechank@gmail.com>
2021-02-04 13:50:44 +0900

    wip: add cache to endpoints

--- src/service/PetService.ts
+++ src/service/PetService.ts
@@ -15,6 +15,8 @@ import {
 } from '../repository/UserPetHistoryRepository';
 import { NestedUserHistoryOfPet } from '../model/UserHistoryOfPet';
 import { ApiError, Summary } from '../common/error';
+import { clearUndefinedFields } from '../common/utils';
+import * as RedisCache from '../redis';
 
 const petRepository = new PetRepository();
 const userPetHistoryRepository = new UserPetHistoryRepository();
@@ -22,19 +24,52 @@ const userPetHistoryRepository = new UserPetHistoryRepository();
 const findAll = async (
     options: PetFindAllOptions
 ): Promise<PetFindAllResult> => {
-    return await petRepository.findAll(options);
+    const tag = 'Pet:FindAll';
+
+    const cached = await RedisCache.read<PetFindAllResult, PetFindAllOptions>({
+        tag,
+        params: options
+    });
+    if (cached !== null) return cached;
+
+    const result = await petRepository.findAll(options);
+    return RedisCache.setAndReturn({
+        tag,
+        params: options,
+        value: result
+    });
 };
 
 const findOne = async (
     id: number,
     options: PetFindOneOptions
 ): Promise<Pet> => {
+    const tag = `Pet(${id}):FindOne`;
+
+    const cached = await RedisCache.read<Pet, PetFindOneOptions>({
+        tag,
+        params: options
+    });
+    if (cached !== null) return cached;
+
     const pet = await petRepository.findOne(id, options);
     if (!pet) throw new ApiError(Summary.NotFound, 'Pet not found');
-    return pet;
+    return RedisCache.setAndReturn({
+        tag,
+        params: options,
+        value: pet
+    });
 };
 
 const findGuardian = async (petID: number): Promise<User | null> => {
+    const tag = `Pet(${petID}):FindGuardian`;
+
+    const cached = await RedisCache.read<User | null, Record<string, never>>({
+        tag,
+        params: {}
+    });
+    if (cached !== null) return cached;
+
     // fetch user history of pet. it should contain one user if user exists currently
     const {
         userHistory
@@ -43,10 +78,19 @@ const findGuardian = async (petID: number): Promise<User | null> => {
     });
 
     // currently, pet does not have user
-    if (userHistory.length === 0) return null;
+    if (userHistory.length === 0)
+        return RedisCache.setAndReturn({
+            tag,
+            params: {},
+            value: null
+        });
 
     const { nickname, userID } = userHistory[0];
-    return { nickname, id: userID };
+    return RedisCache.setAndReturn({
+        tag,
+        params: {},
+        value: { nickname, id: userID }
+    });
 };
 
 const findGuardiansByPetIDs = async (
@@ -59,6 +103,7 @@ const findOneWithGuardian = async (
     id: number,
     options: PetFindOneOptions
 ): Promise<Pet & { guardian: User | null }> => {
+    // these two functions below already use caching
     const pet = await findOne(id, options);
     const guardian = await findGuardian(id);
 
@@ -74,24 +119,31 @@ const createOne = async (fields: PetCreationFields): Promise<number> => {
             'Failed to create pet.'
         );
 
+    await RedisCache.removeBulk([`Pet:FindAll/*`]);
     return insertID;
 };
 
 const updateOne = async (
     id: number,
-    fields: PetModifiableFields
+    _fields: PetModifiableFields
 ): Promise<Pet> => {
-    const petExists = (await petRepository.findOne(id, {})) !== undefined;
+    const pet = await petRepository.findOne(id, {});
+    const petExists = pet !== undefined;
     if (!petExists) throw new ApiError(Summary.NotFound, 'Pet not found');
 
+    const fields = clearUndefinedFields(_fields);
+
     await petRepository.updateOne(id, fields);
-    return { id, ...fields };
+    await RedisCache.removeBulk([`Pet(${id}):*`, 'Pet:FindAll*']);
+
+    return { ...(pet as Pet), ...fields }; // type assertion; pet must not be undefined
 };
 
 const removeOne = async (id: number): Promise<void> => {
     const petExists = (await petRepository.findOne(id, {})) !== undefined;
     if (!petExists) throw new ApiError(Summary.NotFound, 'Pet not found');
 
+    await RedisCache.removeBulk([`Pet(${id}):*`, 'Pet:FindAll*']);
     await petRepository.removeOne(id);
 };
 
@@ -99,10 +151,27 @@ const findUserHistory = async (
     petID: number,
     options: FindHistoryOptions
 ): Promise<FindUserHistoryResult> => {
-    return await userPetHistoryRepository.findUserHistoryFromPetID(
+    const tag = `Pet(${petID}):FindUserHistory`;
+
+    const cached = await RedisCache.read<
+        FindUserHistoryResult,
+        FindHistoryOptions
+    >({
+        tag,
+        params: options
+    });
+    if (cached !== null) return cached;
+
+    const result = await userPetHistoryRepository.findUserHistoryFromPetID(
         petID,
         options
     );
+
+    return RedisCache.setAndReturn({
+        tag,
+        params: options,
+        value: result
+    });
 };
 
 const findUsersByPetIDs = async (
--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -12,6 +12,7 @@ import {
 } from '../repository/UserPetHistoryRepository';
 import { NestedPetHistoryOfUser } from '../model/PetHistoryOfUser';
 import { ApiError, Summary } from '../common/error';
+import * as RedisCache from '../redis';
 
 const userRepository = new UserRepository();
 const userPetHistoryRepository = new UserPetHistoryRepository();
@@ -19,29 +20,69 @@ const userPetHistoryRepository = new UserPetHistoryRepository();
 const findAll = async (
     options: UserFindAllOptions
 ): Promise<UserFindAllResult> => {
-    return await userRepository.findAll(options);
+    const tag = 'User:FindAll';
+
+    const cached = await RedisCache.read<UserFindAllResult, UserFindAllOptions>(
+        { tag, params: options }
+    );
+    if (cached !== null) return cached;
+
+    const result = await userRepository.findAll(options);
+    return await RedisCache.setAndReturn({
+        tag,
+        params: options,
+        value: result
+    });
 };
 
 const findOne = async (
     id: number,
     options: UserFindOneOptions
 ): Promise<User> => {
+    const tag = `User(${id}):FindOne`;
+
+    const cached = await RedisCache.read<User, UserFindOneOptions>({
+        tag,
+        params: options
+    });
+    if (cached !== null) return cached;
+
     const user = await userRepository.findOne(id, options);
     if (!user) throw new ApiError(Summary.NotFound, 'User not found');
-    return user;
+
+    return await RedisCache.setAndReturn({ tag, params: options, value: user });
 };
 
 const findPetHistory = async (
     userID: number,
     options: FindHistoryOptions & { all?: boolean }
 ): Promise<FindPetHistoryResult> => {
+    const tag = `User(${userID}):FindPetHistory`;
     const { all = false, ...restOptions } = options;
+
+    const cached = await RedisCache.read<
+        FindPetHistoryResult,
+        FindHistoryOptions
+    >({
+        tag,
+        params: options
+    });
+    if (cached !== null) return cached;
 
     // query unreleased pets if `all` is false. an empty object queries everything
     const where = all ? {} : { released: 0 };
-    return await userPetHistoryRepository.findPetHistoryFromUserID(userID, {
-        where,
-        ...restOptions
+    const result = await userPetHistoryRepository.findPetHistoryFromUserID(
+        userID,
+        {
+            where,
+            ...restOptions
+        }
+    );
+
+    return await RedisCache.setAndReturn({
+        tag,
+        params: options,
+        value: result
     });
 };
 
@@ -67,6 +108,7 @@ const createOne = async (nickname: string): Promise<number> => {
             'Failed to create user'
         );
 
+    await RedisCache.removeBulk([`User:FindAll/*`]);
     return insertID;
 };
 
@@ -77,6 +119,8 @@ const updateOne = async (id: number, newNickname: string): Promise<User> => {
     await userRepository.updateOne(id, {
         nickname: newNickname
     });
+
+    await RedisCache.removeBulk([`User(${id}):*`, 'User:FindAll*']);
     // return modified entry
     return { id, nickname: newNickname };
 };
@@ -85,6 +129,7 @@ const removeOne = async (id: number): Promise<void> => {
     const userExists = (await userRepository.findOne(id, {})) !== undefined;
     if (!userExists) throw new ApiError(Summary.NotFound, 'User not found');
 
+    await RedisCache.removeBulk([`User(${id}):*`, 'User:FindAll*']);
     await userRepository.removeOne(id);
 };
 
@@ -108,6 +153,8 @@ const registerPet = async (petID: number, userID: number): Promise<void> => {
     await userPetHistoryRepository.insertOne(petID, userID).catch(_ => {
         throw new ApiError(Summary.NotFound, 'Pet or User does not exist!');
     });
+
+    await RedisCache.removeBulk([`User(${userID}):*`, `Pet(${petID}):*`]);
 };
 
 const unregisterPet = async (petID: number, userID: number): Promise<void> => {
